73ad0c1b74d7497550a161beb511c3424c06be5c
Mingkuan Xu <mingkuan.xu@duke.edu>
2019-01-29 13:51:13 -0500

    Replace the original file.

--- bin/snATAC_bmat.py
+++ bin/snATAC_bmat.py
@@ -12,10 +12,14 @@ import pybedtools
 import gzip
 import bz2
 from itertools import islice
+import threading
+from time import ctime, sleep
 
 # process one million reads at a time
-CHUNK_SIZE=1000000
-mat = np.zeros((0, 0))
+CHUNK_SIZE = 1000000
+# CHUNK_SIZE = 10000
+coordinates = set()
+MAX_THREAD = 120
 
 magic_dict = {
     "\x1f\x8b\x08": "gz",
@@ -48,82 +52,8 @@ def next_n_lines(file_opened, N):
     """
     return [x for x in islice(file_opened, N)]
 
-def chunkIt(seq, num):
-    """
-    seperate the list into every two pair
-    i.g. [1,2,3,4,5,6,7,8] -> [[1,2], [3,4], [5,6], [7,8]]
-    every two read as a file
-    """
-    avg = len(seq) / float(num)
-    out = []
-    last = 0.0
-    while last < len(seq):
-        out.append(seq[int(last):int(last + avg)])
-        last += avg
-    return out
-
-def main():
-    from argparse import ArgumentParser
-    # parameters
-    # parser = ArgumentParser(description='generate binary accessibility matrix')
-    # parser.add_argument('-i', '--input', help='bed file contains read', required=True)
-    # parser.add_argument('-x', '--barcode', help='rows: file contains selected cell barcode', required=True)
-    # parser.add_argument('-y', '--peak', help='columns: file contains selected peaks', required=True)
-    # parser.add_argument('-o', '--output', help='output file', required=True)
-    # options = parser.parse_args()
-    # input parsing
-    read_bed = "../data/p56.rep1.bed_100000"
-    # options.input
-    peak_bed = "../data/p56.rep1.ygi"
-    # options.peak
-    barcode_txt = "../data/p56.rep1.xgi"
-    # options.barcode
-    output =  "result1.txt"
-    #options.output
-    # read peaks and reads
-    peaks = pybedtools.BedTool(peak_bed)
-    reads = pybedtools.BedTool(read_bed)
-
-    # find overlap
-    # ov = peaks.intersect(reads, wa=True,  wb=True)
-
-    regions = {}
-    barcodes = {}
-
-    i = 0; j = 0;
-    with open(peak_bed) as fin:
-        for line in fin:
-            regions['\t'.join(line.split()[:3])] = i
-            i += 1
-
-    with open(barcode_txt) as fin:
-        for line in fin:
-            cur_barcode = line.strip().split()[0]
-            barcodes[cur_barcode] = j
-            j += 1
-
-    global mat
-    mat = np.zeros((j, i))
-
-    peaks = pybedtools.BedTool(peak_bed)
-
-
-    counter = 0
-    fin = open_file(read_bed)
-    while True:
-        chunk = next_n_lines(fin, CHUNK_SIZE)
-        if(len(chunk) == 0): break
-        update_matrix_with_chunk(chunk,regions,barcodes,peaks)
-    fin.close()
-
-    # convert the matrix to a binary matrix
-    # mat[ np.where( mat > 1 ) ] = 1
-
-    np.savetxt(output, mat, delimiter='\t', fmt="%d")
-
-
-
-def update_matrix_with_chunk(chunk,regions,barcodes,peaks):
+def find_coordinates(chunk,peaks,regions,barcodes):
+    # print("Handling the chunk at %s" % ctime())
     reads = pybedtools.BedTool(chunk)
     for line in peaks.intersect(reads, wa=True, wb=True):
         elems = str(line).split()
@@ -131,8 +61,81 @@ def update_matrix_with_chunk(chunk,regions,barcodes,peaks):
         cur_barcode = elems[6]
         if cur_barcode not in barcodes: continue
         if cur_region not in regions: sys.exit("error(main): region not in the list")
-        mat[barcodes[cur_barcode], regions[cur_region]] = 1
+        coordinates.add((barcodes[cur_barcode], regions[cur_region]))
 
+def create_barcode_hash(barcode_txt):
+    """
+    Create a hash table for the barcodes
+    """
+    barcodes = {}
+    i = 0
+    with open(barcode_txt) as fin:
+        for line in fin:
+            cur_barcode = line.strip().split()[0]
+            barcodes[cur_barcode] = i
+            i += 1
+    return barcodes
+
+def create_region_hash(peak_bed):
+    """
+    Create a hash table for the regions
+    """
+    regions = {}
+    i = 0;
+    with open(peak_bed) as fin:
+        for line in fin:
+            regions['\t'.join(line.split()[:3])] = i
+            i += 1
+    return regions
+
+def update_matrix_with_coordinates(coordinates,i,j):
+    mat = np.zeros((j, i))
+    for each in coordinates:
+        mat[each[0], each[1]] = 1
+    return mat
+
+def main():
+    from argparse import ArgumentParser
+    # parameters
+    parser = ArgumentParser(description='generate binary accessibility matrix')
+    parser.add_argument('-i', '--input', help='bed file contains read', required=True)
+    parser.add_argument('-x', '--barcode', help='rows: file contains selected cell barcode', required=True)
+    parser.add_argument('-y', '--peak', help='columns: file contains selected peaks', required=True)
+    parser.add_argument('-o', '--output', help='output file', required=True)
+    options = parser.parse_args()
+    # input parsing
+    read_bed = options.input
+    peak_bed = options.peak
+    barcode_txt = options.barcode
+    output =  options.output
+
+    # read peaks and reads
+    peaks = pybedtools.BedTool(peak_bed)
+    reads = pybedtools.BedTool(read_bed)
+
+    # create hash tables for peaks and barcodes
+    barcodes = create_barcode_hash(barcode_txt)
+    regions = create_region_hash(peak_bed)
+
+    fin = open_file(read_bed)
+
+    while True:
+        # chunk1 = next_n_lines(fin, CHUNK_SIZE)
+        chunk1 = next_n_lines(fin, CHUNK_SIZE)
+        if(len(chunk1) == 0): break
+        # find_coordinates(chunk1,peaks,regions,barcodes)
+        t1 = threading.Thread(target = find_coordinates,args=(chunk1,peaks,regions,barcodes,))
+        t1.start()
+        if(threading.activeCount()>MAX_THREAD):
+            t1.join()
+        # print("Current threading is %d" % threading.activeCount())
+
+    fin.close()
+    t1.join()
+
+    matrix = update_matrix_with_coordinates(coordinates,len(regions),len(barcodes))
+
+    np.savetxt(output, matrix, delimiter='\t', fmt="%d")
 
 if __name__ == '__main__':
     main()
